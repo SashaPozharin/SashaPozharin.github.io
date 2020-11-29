2522929e86e831c57d1b2c12dc2ae1c94be69b30
SashaPozharin <68804550+SashaPozharin@users.noreply.github.com>
2020-11-29 14:48:39 +0300

    ура новые изменения

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 
-
-
 $(document).ready(function () {
     // включаем tolltip у элемента
     $("[data-toggle='tooltip']").tooltip();
     $("[data-toggle='popover']").popover();
+
+    //Формируем запрос
+    $(".modal-body").load("https://artart241.github.io img.d-block.w-100", function (response, status, request) {
+        
+    });
+
 });
+
+$("#btnSelect").click(function(e) {
+    console.log($("option[selected]"));
+});
+
